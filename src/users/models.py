8e337a6fbb28926800e55bc05c9bb84084f6f5bc
Georg <georg.vietzke@gmail.com>
2024-08-27 15:34:52 +0200

    SC-22 feat: custom user model
    this inlcudes an address table
    
    Co-authored-by: Simon Antonius Lauer <simon.lauer@posteo.de>

--- src/users/models.py
+++ src/users/models.py
@@ -1,6 +1,28 @@
-# from django.db import models
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+class Address(models.Model):
+    city = models.CharField(max_length=128)
+    postel_code = models.CharField(max_length=16)
+
+    def __str__(self):
+        return self.city
+
+
 class User(AbstractUser):
-    pass
+    helper = models.BooleanField(default=False)
+    seeker = models.BooleanField(default=False)
+    email_verified = models.BooleanField(default=False)
+    address_id = models.OneToOneField(
+        Address,
+        on_delete=models.CASCADE,
+        null=True,
+    )
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    
+
+
