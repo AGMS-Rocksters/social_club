7cab11ce4745415e9c98494acdd1ddfc5226b294
ArezouRa <arezou.rgh@gmail.com>
2024-08-27 13:52:32 +0200

    feat: creating models for posts and comments Co-authored-by: Mohammed Abu dunya <mohammed.yahia2020@gmail.com>

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(
+        'users.User', 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        related_name='posts'
+    )
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(
+        'users.User', 
+        on_delete=models.CASCADE, 
+        related_name='comments'
+    )
+    post = models.ForeignKey(
+        'posts.Post', 
+        on_delete=models.CASCADE, 
+        related_name='comments'
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    upvotes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'Comment by {self.user} on {self.post}'
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['created_at']),
+        ]
