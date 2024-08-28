8796a5206ea58ec93a06079dd93be8adee0dc104
Georg <georg.vietzke@gmail.com>
2024-08-28 12:01:11 +0200

    SC-22 fix: fix address and user table

--- src/users/models.py
+++ src/users/models.py
@@ -4,17 +4,16 @@ from django.contrib.auth.models import AbstractUser
 
 class Address(models.Model):
     city = models.CharField(max_length=128)
-    postel_code = models.CharField(max_length=16)
+    postal_code = models.CharField(max_length=16, null=True, blank=True)
 
     def __str__(self):
         return self.city
-
 
 class User(AbstractUser):
     helper = models.BooleanField(default=False)
     seeker = models.BooleanField(default=False)
     email_verified = models.BooleanField(default=False)
-    address_id = models.OneToOneField(
+    address = models.OneToOneField(
         Address,
         on_delete=models.CASCADE,
         null=True,
