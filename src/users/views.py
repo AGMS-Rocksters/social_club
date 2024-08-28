6a5d9d865034aa03397bdffcf44234a916c2c17b
ArezouRa <arezou.rgh@gmail.com>
2024-08-28 15:22:56 +0200

    SC-33 feat: creating serializer, urls and views to be completed
    Co-authored-by: Georg Vietzke <georg.vietzke@gmail.com>
    Mohammad Abu Dunya <mohammed.yahia2020@gmail.com>

--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from users.models import User
+from users.serializers import UserSerializer
+from rest_framework import generics, permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+    
