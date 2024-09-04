a716e07fa7a0696fd18c43b7705605fe722d8387
Georg <georg.vietzke@gmail.com>
2024-09-04 11:40:32 +0200

    SC-37 fix: delete swagger stuff

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,16 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Social Platform API",
-        default_version="1.0.0",
-        description="API documentation",
-    )
-)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
