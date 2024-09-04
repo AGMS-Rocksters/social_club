06ba78978f7947b45553e8b12b0de03d4fe444df
Georg <georg.vietzke@gmail.com>
2024-09-04 11:54:12 +0200

    SC-46	feat: swagger ou set up

--- src/config/urls.py
+++ src/config/urls.py
@@ -18,8 +18,26 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Social Platform API",
+        default_version="1.0.0",
+        description="API documentation",
+    ),
+    public=True,
+)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "swagger/schema/",
+        schema_view.with_ui("swagger", chache_timeout=0),
+        name="swagger_schema",
+    ),
     path("api/users/", include("users.urls", namespace="users")),
     path("api/posts/", include("posts.urls")),
 ]
